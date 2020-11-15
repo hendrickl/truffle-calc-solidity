71dfd6c53208b0349c59bb4176f36f1c2e1a23c2
hendrick <h.lincertin@gmail.com>
2020-11-15 01:13:05 +0100

    migrations updated

---
+++ migrations/_2_deploy_calc_operations.js
@@ -0,0 +1,15 @@
+// Adder is an artifact of the Adder contract
+const Adder = artifacts.require("Adder");
+// Suber is an artifact of the Suber contract
+const Suber = artifacts.require("Suber");
+// Multiplier is an artifact of the Multiplier contract
+const Multiplier = artifacts.require("Multiplier");
+// Divisor is an artifact of the Multiplier contract
+const Divisor = artifacts.require("Divisor");
+
+module.exports = async (deployer) => {
+  await deployer.deploy(Adder);
+  await deployer.deploy(Suber);
+  await deployer.deploy(Multiplier);
+  await deployer.deploy(Divisor);
+};
